f392b543dd5d62dba60b6b74d08bbec6aac977e1
Vinícius Giovanini <vinicius.giovanini@vibelink.ai>
2025-08-06 18:12:17 -0300

    add owner e repo name

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,12 @@ echo "Iniciando Action..."
 # CI_REGISTRY_PASSWORD="$INPUT_CI_REGISTRY_PASSWORD"
 BRANCH_NAME="$INPUT_BRANCH_NAME"
 GH_TOKEN="$INPUT_GITHUB_TOKEN"
+GH_OWNER="$INPUT_GITHUB_OWNER"
+GH_REPO="$INPUT_GITHUB_REPO_NAME"
 export GH_TOKEN
-echo "GH_TOKEN recebido: ${GH_TOKEN:0:5}******"
+export GH_OWNER
+export GH_REPO
+echo "GH_TOKEN recebido: ${GH_TOKEN:0:5}****** e ${GW_REPO} e ${GH_OWNER}"
 
 # echo "$CI_REGISTRY_PASSWORD" | docker login -u "$CI_REGISTRY_USER" --password-stdin "$CI_REGISTRY"
 
