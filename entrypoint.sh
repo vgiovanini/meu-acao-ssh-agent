85b8975a10c7eb2447688865021ed808acdece91
Vinicius Giovanini <vinicius.giovanini@vibelink.ai>
2025-08-07 09:16:46 -0300

    alterando para dar output do codigo

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,9 +13,13 @@ export GH_REPO
 echo "GH_TOKEN recebido: ${GH_TOKEN:0:5}****** e ${GW_REPO} e ${GH_OWNER}"
 
 if [[ "$BRANCH_NAME" == "homolog" ]]; then
-  TAG=$(python3 /scripts/main.py --prerelease | grep "TAG:" | awk '{print $2}')
+  output=$(python3 /scripts/main.py --prerelease)
+  echo "$output"
+  TAG=$(echo "$output"| grep "TAG:" | awk '{print $2}')
 elif [[ "$BRANCH_NAME" == "master" ]]; then
-  TAG=$(python3 /scripts/main.py --release | grep "TAG:" | awk '{print $2}')
+  output=$(python3 /scripts/main.py --release)
+  echo "$output"
+  TAG=$(echo "$output"| grep "TAG:" | awk '{print $2}')
 else
   echo "Branch $BRANCH_NAME não suportada para release"
   exit 1
