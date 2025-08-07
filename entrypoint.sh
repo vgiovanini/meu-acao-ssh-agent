60972b83f57d5d65678057db4c9094db920bdaca
Vinicius Giovanini <vinicius.giovanini@vibelink.ai>
2025-08-07 09:41:23 -0300

    voltando com os exports

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ BRANCH_NAME="$INPUT_BRANCH_NAME"
 GH_TOKEN="$INPUT_GITHUB_TOKEN"
 GH_OWNER="$INPUT_GITHUB_OWNER"
 GH_REPO="$INPUT_GITHUB_REPO_NAME"
-# export GH_TOKEN
-# export GH_OWNER
-# export GH_REPO
-echo "GH_TOKEN recebido: ${GH_TOKEN:0:5}****** e ${GW_REPO} e ${GH_OWNER}"
+export GH_TOKEN
+export GH_OWNER
+export GH_REPO
+echo "GH_TOKEN recebido: ${GH_TOKEN:0:5}****** e ${GH_REPO} e ${GH_OWNER}"
 
 if [[ "$BRANCH_NAME" == "homolog" ]]; then
   output=$(python3 /scripts/main.py --prerelease)
